5f61ac7c695f1cadf80ee20959300ef43e950dd0
ali1373107 <alidashti1994a@gmail.com>
2023-02-11 15:21:22 +0000

    100

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  
   makePageForEpisodes(allEpisodes);
 }
 
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const allEpisodesEl = document.getElementById("allEpisodes");
+  episodeList.forEach(episode =>{
+    const epDiv = document.createElement("div");
+    epDiv.classList.add("episodes");
+    const p = document.createElement("p");
+    
+    p.innerText = episode.name;
+    epDiv.appendChild(p);
+    
+    const img = document.createElement("img");
+    img.src = episode.image.medium;
+    img.classList.add("epImg")
+    epDiv.appendChild(img);
+    epDiv.innerHTML+=episode.summary;
+
+
+
+    allEpisodesEl.appendChild(epDiv);
+  })
+  console.log(episodeList);
 }
 
 window.onload = setup;
