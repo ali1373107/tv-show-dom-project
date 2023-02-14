df779e6710fd81f218b5712153782d8bb4e8c5f8
ali1373107 <alidashti1994a@gmail.com>
2023-02-14 13:20:35 +0000

    200

--- script.js
+++ script.js
@@ -5,15 +5,25 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function makeSeasonAndEpisode(episode){
+  const {season, number} = episode;
+  const paddedSeason = season.toString().padStart(2, "0");
+  const paddedEpisode = number.toString().padStart(2, "0");
+  return `S${paddedSeason}E${paddedEpisode}`;
+}
 
 function makePageForEpisodes(episodeList) {
   const allEpisodesEl = document.getElementById("allEpisodes");
+  allEpisodesEl.innerHTML = "";
+  const countParagraph = document.createElement("p");
+  countParagraph.innerText = `Showing ${episodeList.length} episodes`
+  allEpisodesEl.appendChild(countParagraph);
   episodeList.forEach(episode =>{
     const epDiv = document.createElement("div");
     epDiv.classList.add("episodes");
     const p = document.createElement("p");
     
-    p.innerText = episode.name;
+    p.innerText =`${makeSeasonAndEpisode(episode)} ${episode.name}`;
     epDiv.appendChild(p);
     
     const img = document.createElement("img");
@@ -28,5 +38,15 @@ function makePageForEpisodes(episodeList) {
   })
   console.log(episodeList);
 }
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input",(event) => {
+const searchString = event.target.value.toLowerCase();
+  const filteredEpisodes = getAllEpisodes().filter((episode)=> {
+    return (episode.summary.toLowerCase().includes(searchString) || 
+            episode.name.toLowerCase().includes(searchString));
+ 
+  });
+  makePageForEpisodes(filteredEpisodes)
+});
 
 window.onload = setup;
