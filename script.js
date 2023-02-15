1aa3991d83b55ba52383d17870b3ddab1ab112b1
ali1373107 <alidashti1994a@gmail.com>
2023-02-15 12:46:21 +0000

    300 done

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //You can edit ALL of the code here
+const url = "https://api.tvmaze.com/shows/82/episodes"
+let allEpisodes = []
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  
-  makePageForEpisodes(allEpisodes);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+     
+      allEpisodes = data;
+      makePageForEpisodes(allEpisodes);
+    })
+    .catch((err) => console.error(err));
 }
 
 function makeSeasonAndEpisode(episode){
@@ -14,6 +22,9 @@ function makeSeasonAndEpisode(episode){
 
 function makePageForEpisodes(episodeList) {
   const allEpisodesEl = document.getElementById("allEpisodes");
+
+  const selectElem = document.getElementById("select-input");
+  console.log(selectElem);
   allEpisodesEl.innerHTML = "";
   const countParagraph = document.createElement("p");
   countParagraph.innerText = `Showing ${episodeList.length} episodes`
@@ -35,18 +46,33 @@ function makePageForEpisodes(episodeList) {
 
 
     allEpisodesEl.appendChild(epDiv);
+    const option = document.createElement("option");
+    option.textContent = `${makeSeasonAndEpisode(episode)} - ${episode.name}`;
+    option.value = episode.id;
+    selectElem.appendChild(option);
   })
   console.log(episodeList);
 }
 const searchInput = document.getElementById("search-input");
 searchInput.addEventListener("input",(event) => {
 const searchString = event.target.value.toLowerCase();
-  const filteredEpisodes = getAllEpisodes().filter((episode)=> {
+  const filteredEpisodes = allEpisodes.filter((episode)=> {
     return (episode.summary.toLowerCase().includes(searchString) || 
             episode.name.toLowerCase().includes(searchString));
  
   });
   makePageForEpisodes(filteredEpisodes)
 });
+const selectInput = document.getElementById("select-input");
+selectInput.addEventListener("change",(e) => {
+  const idSelectedByUser = Number(e.target.value);
+  console.log({idSelectedByUser});
+  const selectedEpisode = allEpisodes.find(
+    (ep)=> ep.id ===idSelectedByUser);
+    if(selectedEpisode){
+      makePageForEpisodes([selectedEpisode]);
+    }
+});
+
 
 window.onload = setup;
